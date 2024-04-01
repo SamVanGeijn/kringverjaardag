a2b6f4b1020de615a320d794ff6c3579498ba979
SamVanGeijn <pluisam@gmail.com>
2024-04-01 12:20:15 +0200

    Added a beautiful and simple chat.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
 
+  @ViewChild('chatInput') chatInput!: ElementRef;
+
+  messages: message[] = [];
+
+  constructor() { }
+
+  sendMessage(message: string) {
+    this.messages.push({ id: this.messages.length + 1, text: message, fromUser: true });
+    this.messages.push({ id: this.messages.length + 1, text: "< ANTWOORD >", fromUser: false });
+    this.chatInput.nativeElement.value = "";
+  }
+
+}
+
+export interface message {
+  id: number;
+  text: string;
+  fromUser: boolean;
 }
